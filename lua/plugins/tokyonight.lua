d3ce604076b6325e3979d0642bb26217dae3de87
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-08-22 09:11:33 +0530

    update tokyonight theme

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,10 @@
+-- tokyonight.nvim
+-- https://github.com/folke/tokyonight.nvim#%EF%B8%8F-configuration
+
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "night",
+    floats = "transparent",
+  },
+}
