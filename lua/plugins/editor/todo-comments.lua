fe53e3450f2872fb19aea2c0dceacd11cc75c544
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-03 19:25:07 +0530

    add: plugin todo-comments

---
+++ lua/plugins/editor/todo-comments.lua
@@ -0,0 +1,20 @@
+-- todo-comments.nvim
+-- https://github.com/folke/todo-comments.nvim
+-- Finds and lists all of the  TODO, HACK, BUG, etc comment
+-- in your project and loads them into a browsable list.
+
+return {
+  "folke/todo-comments.nvim",
+  cmd = { "TodoTrouble", "TodoTelescope" },
+  event = { "BufReadPost", "BufNewFile" },
+  config = true,
+  -- stylua: ignore
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+    { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+    { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+    { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+  },
+}
