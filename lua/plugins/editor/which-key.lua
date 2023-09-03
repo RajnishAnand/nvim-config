d0ec6d3c3cd63377a2696a3c3217c46cccfb0466
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-03 13:50:01 +0530

    update: removed which-key initial keymaps

--- lua/plugins/editor/which-key.lua
+++ lua/plugins/editor/which-key.lua
@@ -5,22 +5,7 @@ return {
     plugins = { spelling = true },
     defaults = {
       mode = { "n", "v" },
-      ["g"] = { name = "+goto" },
-      ["gz"] = { name = "+surround" },
-      ["]"] = { name = "+next" },
-      ["["] = { name = "+prev" },
-      ["<leader><tab>"] = { name = "+tabs" },
-      ["<leader>b"] = { name = "+buffer" },
-      ["<leader>c"] = { name = "+code" },
-      ["<leader>f"] = { name = "+file/find" },
-      ["<leader>g"] = { name = "+git" },
-      ["<leader>gh"] = { name = "+hunks" },
-      ["<leader>q"] = { name = "+quit/session" },
-      ["<leader>s"] = { name = "+search" },
-      ["<leader>u"] = { name = "+ui" },
-      ["<leader>w"] = { name = "+windows" },
-      ["<leader>x"] = { name = "+diagnostics/quickfix" },
-    },
+   },
   },
   config = function(_, opts)
     local wk = require("which-key")
