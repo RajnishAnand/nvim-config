05db8519d27bd31d89b78a3abc6d1e26d23e791b
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-03 13:47:20 +0530

    update: configured for default lazy load

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,11 +20,11 @@ require("lazy").setup({
     -- { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
+    lazy = true, -- all plugins to be lazyloaded by default
+
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
+
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
