0f8fab400a2ee0f07f307d5b478de2bfe082487d
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-03 13:48:16 +0530

    add: gitsigns.nvim

---
+++ lua/plugins/editor/gitsigns.lua
@@ -0,0 +1,73 @@
+-- gitsigns.nvim
+-- https://github.com/lewis6991/gitsigns.nvim
+
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+
+    signs = {
+      add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+      change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      delete = { hl = "GitSignsDelete", text = "▶", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+      topdelete = { hl = "GitSignsDelete", text = "▶", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+      changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    },
+
+    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    watch_gitdir = { interval = 1000, follow_files = true },
+    attach_to_untracked = true,
+    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 1000,
+      ignore_whitespace = false,
+    },
+
+    current_line_blame_formatter_opts = { relative_time = false },
+    sign_priority = 6,
+    update_debounce = 100,
+    status_formatter = nil, -- Use default
+    max_file_length = 40000,
+
+    -- Options passed to nvim_open_win
+    preview_config = {
+      border = "single",
+      style = "minimal",
+      relative = "cursor",
+      row = 0,
+      col = 1,
+    },
+    yadm = { enable = false },
+
+    on_attach = function(buffer)
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+      -- stylua: ignore start
+      -- map("n", "]h", gs.next_hunk, "Next Hunk")
+      -- map("n", "[h", gs.prev_hunk, "Prev Hunk")
+      -- map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      -- map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      -- map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+      -- map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      -- map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+      -- map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
+      -- map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+      -- map("n", "<leader>ghd", gs.diffthis, "Diff This")
+      -- map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+      -- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+    end,
+  }
+}
+
+
+
+
