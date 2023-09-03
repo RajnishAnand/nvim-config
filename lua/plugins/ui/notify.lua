fa30887ad0bba1311fc4c23bcf9241808ea756b8
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-03 20:49:16 +0530

    update: notify setup

--- lua/plugins/ui/notify.lua
+++ lua/plugins/ui/notify.lua
@@ -1,19 +1,32 @@
 -- nvim notify
 -- https://github.com/rcarriga/nvim-notify
+-- notification 
 
 return {
   "rcarriga/nvim-notify",
   opts = {
     background_colour = "#000000",
     render = "compact",
-    stages = "static",
+    stages = "static"
+  },
 
-    icons = {
-      DEBUG = "",
-      ERROR = "",
-      INFO = "",
-      TRACE = "✎",
-      WARN = "",
+  keys = {
+    {
+      "<leader>un",
+      function()
+        require("notify").dismiss({ silent = true, pending = true })
+      end,
+      desc = "Dismiss all Notifications",
     },
   },
+  opts = {
+    timeout = 3000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
+  },
+  init = function()  vim.notify = require("notify") end,
 }
