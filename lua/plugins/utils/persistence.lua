bd488f157f005a5413193fd9abdd8ec20727bb18
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-07 16:54:51 +0530

    add: plugin persistence.nvim

---
+++ lua/plugins/utils/persistence.lua
@@ -0,0 +1,14 @@
+-- persistence
+-- https://github.com/folke/persistence.nvim
+
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  opts = {
+    options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
+    pre_save = nil, -- a function to call before saving the session
+  },
+
+  -- automatically loads last session
+  init = function () require("persistence").load() end
+}
