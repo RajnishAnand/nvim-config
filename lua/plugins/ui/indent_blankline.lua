deb4879de408c87de3f1b86f6c564a7ecc74f121
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-09 20:31:50 +0530

    update: disabled curentlineinent start

--- lua/plugins/ui/indent_blankline.lua
+++ lua/plugins/ui/indent_blankline.lua
@@ -23,6 +23,6 @@ return {
     show_end_of_line = true,
     space_char_blankline = " ",
     show_current_context = true,
-    show_current_context_start = true,
+    -- show_current_context_start = true,
   }
 }
