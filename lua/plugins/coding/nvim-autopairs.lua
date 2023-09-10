c00584d51003b6d7eb0377d4a4cdb89962d2235f
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-10 08:04:02 +0530

    add: plugin nvim-autopairs

---
+++ lua/plugins/coding/nvim-autopairs.lua
@@ -0,0 +1,8 @@
+-- nvim-autopairs
+-- https://github.com/windwp/nvim-autopairs
+
+return {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equalent to setup({}) function
+}
