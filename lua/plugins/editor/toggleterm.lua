1a52c6e26a7785eeb04424e4c346249541a1bc32
RajnishAnand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-13 22:38:02 +0530

    add: plugin toggleterm initial config

---
+++ lua/plugins/editor/toggleterm.lua
@@ -0,0 +1,10 @@
+-- toggleterm.nvim
+-- https://github.com/akinsho/toggleterm.nvim
+
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  opts = {
+    --[[ things you want to change go here]]
+  }
+}
