475b5c346b741195565b6ae8432fcde753f90be7
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-15 23:22:38 +0530

    fix: disabled debug log level

--- lua/plugins/utils/presence.lua
+++ lua/plugins/utils/presence.lua
@@ -11,7 +11,7 @@ return {
     auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
     neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
     main_image          = "file",                   -- Main image display (either "neovim" or "file")
-    log_level           = "debug",                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
     debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
     enable_line_number  = false,                      -- Displays the current line number instead of the current project
     buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
