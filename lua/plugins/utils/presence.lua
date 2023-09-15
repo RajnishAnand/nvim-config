c9c3a329475182fb793a10bda35c563c32130456
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-15 23:19:06 +0530

    fix: config issue

--- lua/plugins/utils/presence.lua
+++ lua/plugins/utils/presence.lua
@@ -11,12 +11,10 @@ return {
     auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
     neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
     main_image          = "file",                   -- Main image display (either "neovim" or "file")
-    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    log_level           = "debug",                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
     debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
     enable_line_number  = false,                      -- Displays the current line number instead of the current project
-    blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
     buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
-    file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
     show_time           = false,                       -- Show the timer
 
     -- Rich Presence text options
@@ -29,5 +27,5 @@ return {
     line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
   },
 
-  config = function() require("presence").setup(opts) end 
+  config = function(_, opts) require("presence").setup(opts) end 
 }
