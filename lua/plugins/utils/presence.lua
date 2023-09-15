c4dfdb0b9ce9440cdb89ad180bccf1e3c58d1c12
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-09-15 22:26:07 +0530

    fix: presence config

--- lua/plugins/utils/presence.lua
+++ lua/plugins/utils/presence.lua
@@ -4,19 +4,20 @@
 
 return {
   "andweeb/presence.nvim",
+  event = "VeryLazy",
+  
   opts = {
      -- General options
     auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
     neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
-    main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
-    client_id           = "800445583046213663",       -- Use your own Discord application client id (not recommended)
+    main_image          = "file",                   -- Main image display (either "neovim" or "file")
     log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
     debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
     enable_line_number  = false,                      -- Displays the current line number instead of the current project
     blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
     buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
     file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-    show_time           = true,                       -- Show the timer
+    show_time           = false,                       -- Show the timer
 
     -- Rich Presence text options
     editing_text        = "Editing %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
@@ -28,7 +29,5 @@ return {
     line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
   },
 
-  init = function () 
-    require ("presence").setup(opts) 
-  end
+  config = function() require("presence").setup(opts) end 
 }
