d35a4e1d7c99977d22430ba1e5bd268a1c8029e2
Rajnish Anand <63791262+RajnishAnand@users.noreply.github.com>
2023-12-06 21:25:26 +0530

    update: nvim tree glyphs git icons

--- lua/plugins/editor/nvim-tree.lua
+++ lua/plugins/editor/nvim-tree.lua
@@ -91,7 +91,7 @@ return {
                   ignored = "◌",
                   -- change type
                   renamed = "➜",
-                  deleted = "󰧧",
+                  deleted = "",
                 },
             },
           },
